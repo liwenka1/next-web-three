4ea963e29968355c0210c469217d7a30aa38fa07
unknown <2020583117@qq.com>
2025-04-15 23:02:42 +0800

    feat: 完成一个three.js最简单的demo

---
+++ app/(site)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+
+import Box from "./components/box";
+
+const Site = () => {
+  return (
+    <Canvas style={{ height: 900 }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+};
+
+export default Site;
